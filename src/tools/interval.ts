de275d88f60937efd600a7ac0bb286fef6aecb75
000wan <87213416+000wan@users.noreply.github.com>
2022-11-19 01:21:07 +0900

    Complete migrating to TypeScript

--- src/tools/interval.ts
+++ src/tools/interval.ts
@@ -2,15 +2,16 @@
 // Ref: https://velog.io/@yeyo0x0/React-React-Hooks%EC%97%90%EC%84%9C-setInterval-%EC%82%AC%EC%9A%A9-%EB%AC%B8%EC%A0%9C
 import { useEffect, useRef } from "react";
 
-const useInterval = (callback, delay) => {
-  const savedCallback = useRef();
+const useInterval = (callback: ()=>any, delay: number) => {
+  const savedCallback = useRef<()=>any>();
+
   useEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
 
   useEffect(() => {
     function tick() {
-      savedCallback.current();
+      if(savedCallback.current) savedCallback.current();
     }
     if (delay !== null) {
       let id = setInterval(tick, delay);
