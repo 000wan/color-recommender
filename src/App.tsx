2fefc06e308380f1cd09daddcd111e0bc9f9f78c
000wan <87213416+000wan@users.noreply.github.com>
2022-11-24 05:08:57 +0900

    Create Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from './pages/main';
+import LoginPage from './pages/login';
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/login" element={ <LoginPage/> }/>
           <Route path="/" element={ <MainPage/> }/>
         </Routes>
       </BrowserRouter>
