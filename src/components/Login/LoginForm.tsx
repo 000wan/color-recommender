4b7de1ee8751e74e35b05aa94fc8de1537ea8185
000wan <87213416+000wan@users.noreply.github.com>
2022-11-26 02:28:30 +0900

    Complete Login page
    Feature: Check whether username is duplicated

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './css/LoginForm.css'
 import axios from "axios";
 import { APIBase } from '../../tools/api';
+import { useInterval } from '../../tools/interval';
 
 interface UserCredential {
   username: string,
@@ -10,83 +11,137 @@ interface UserCredential {
 
 interface LoginProps {}
 
-interface LoginState extends UserCredential {
-  submitType: number
-}
+const LoginForm = ( props: LoginProps ) => {
+  // credential
+  const [ username, setUsername ] = useState<string>("");
+  const [ password, setPassword ] = useState<string>("");
+  // etc
+  const [ submitType, setSubmitType ] = useState<number>(0); // 1: Sign-In, 0: Sign-Up
+  const [ authType, setAuthType ] = useState<number>(-1); // 1: Sign-In, 0: Sign-Up, -1: Neither
+  const [ nameOK, setNameOK ] = useState<number>(0); // 1: OK, 0: Not OK, -1: Checking
+  const [ timer, setTimer ] = useState<number>(0);
 
-class LoginForm extends React.Component<LoginProps, LoginState> {
-  constructor(props: LoginProps) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      submitType: 0
-    }
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  signin = async (credential: UserCredential) => {
+  const signin = async (credential: UserCredential) => {
     interface IAPIResponse { loginSuccess: boolean, message: string };
 
     try {
       const { data } = await axios.post<IAPIResponse>(APIBase + "/auth/login", { credential });
-      if(data.loginSuccess) {
-        alert("Login Success!");
+      if( data.loginSuccess ) {
+        alert("Sign-In Success: " + data.message);
       } else {
-        alert("Login Failed!");
+        alert("Sign-In Failed: " + data.message);
       }
     } catch (e: any) {
       const data:IAPIResponse = e.response.data; 
-      alert("[Login Failed] " + data.message);
+      alert(data.message);
     }
   }
 
-  signup = async (credential: UserCredential) => {
-    interface IAPIResponse { message: string };
+  const signup = async (credential: UserCredential) => {
+    interface IAPIResponse { registerSuccess: boolean, message: string };
 
     try {
       const { data } = await axios.post<IAPIResponse>(APIBase + "/auth/register", { credential });
-      alert(data.message);
+      if ( data.registerSuccess ) {
+        alert("Sign-Up Success: " + data.message);
+      } else {
+        alert("Sign-Up Failed: " + data.message);
+      }
     } catch (e: any) {
       const data:IAPIResponse = e.response.data; 
       alert(data.message);
     }
   }
 
-  handleSubmit(event: any) {
-    const credential:UserCredential = {
-      username: this.state.username,
-      password: this.state.password
-    } 
+  const handleSubmit = (event: any) => {
+    const credential:UserCredential = { username, password }; 
 
-    switch(this.state.submitType) {
-      case 0: // Sign-In
-        this.signin(credential);
+    switch( submitType ) {
+      case 1:
+        signin(credential);
         break;
-      case 1: // Sign-Up
-        this.signup(credential);
+      case 0:
+        signup(credential);
         break;
     }
     event.preventDefault();
   }
 
-  render() {
-    return (
-      <div className={"login-form"}>
-        <form onSubmit={this.handleSubmit}>
-          <label className={"login-label"}>Username</label>
-          <input className={"login-input"} type={"text"} value={this.state.username} onChange={e => this.setState({username: e.target.value})}/>
+  // check whether username duplicated
+  const tickTime = 2000; // ms
+  const timerDelay = 1000;
 
-          <label className={"login-label"}>Password (Optional)</label>
-          <input className={"login-input"} type={"password"} value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
-          <br />
-          <button className={"login-button"} type={"submit"} onClick={e => this.setState({submitType: 0})}>Sign-In</button>
-          <button className={"login-button"} type={"submit"} onClick={e => this.setState({submitType: 1})}>Sign-Up</button>
-        </form>
-      </div>
-    );
+  const findUsername = async ( username: string ) => {
+    try {
+      interface IAPIResponse { result: boolean };
+      const { data } = await axios.post<IAPIResponse>(APIBase + "/auth/find-user", { username });
+      if(data.result) {
+        return 1; // Found
+      } else {
+        return 0; // Not found
+      }
+    } catch (e) {
+      return -1; // Error
+    }
   }
+
+  const tick = () => {
+    setTimer(timer => timer + 1);
+
+    if ( !username ) { // forbidden username (empty)
+      setAuthType(-1);
+      setNameOK(0);
+    } else if ( timer >= tickTime/timerDelay && nameOK === -1 ) {
+      // didn't check yet
+      findUsername(username).then(( res ) => {
+        setAuthType(res); // 1: Found => Sign-In, 0: Not found => Sign-Up, -1: Neither available
+        if( res === -1 ) {
+          setNameOK(-1);
+        } else {
+          setNameOK(1);
+        }
+      });
+    }
+    //console.log(timer);
+  }
+
+  useEffect(() => {
+    setAuthType(-1);
+    setNameOK(-1);
+    setTimer(0);
+  }, [ username ]);
+
+  useInterval(tick, timerDelay);
+
+  const [ emoji, setEmoji ] = useState<number>(0);
+  useEffect(() => {
+    switch ( nameOK ) {
+      case -1:
+        setEmoji(0x23F3); // checking
+        break;
+      case 0:
+        setEmoji(0x274C); // not OK
+        break;
+      case 1:
+        setEmoji(0x2705); // OK
+        break;
+    }
+  }, [ nameOK ]);
+
+  return (
+    <div className={"login-form"}>
+      <form onSubmit={ handleSubmit }>
+        <label className={"login-label"}>Username { String.fromCharCode(emoji) }</label>
+        <input className={"login-input"} type={"text"} value={ username } onChange={e => setUsername( e.target.value )} />
+
+        <label className={"login-label"}>Password (Optional)</label>
+        <input className={"login-input"} type={"password"} value={ password } onChange={e => setPassword( e.target.value )} />
+        <br />
+        <button className={"login-button"} type={"submit"} onClick={e => setSubmitType(1)} disabled={authType !== 1}>Sign-In</button>
+        <button className={"login-button"} type={"submit"} onClick={e => setSubmitType(0)} disabled={authType !== 0}>Sign-Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
